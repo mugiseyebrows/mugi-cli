7365cfd940c11f42528c3fbcac0c0ef473622035
Doronin Stanislav <mugisbrows@gmail.com>
2022-02-24 13:52:20 +0300

    ext stat from stdin

--- mugicli/pyextstat.py
+++ mugicli/pyextstat.py
@@ -1,23 +1,36 @@
 import os
 import argparse
 from .shared import glob_paths_dirs
+from . import read_stdin_text
 
 def main():
-    parser = argparse.ArgumentParser(description='prints extension statistics')
-    parser.add_argument("paths", nargs="*", type=str, help="paths")
-    parser.add_argument('-s', '--short', action='store_true', help='short statistics')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", nargs="*", help="paths")
+    parser.add_argument('-s', '--short', action='store_true')
+
     args = parser.parse_args()
-    paths = glob_paths_dirs(args.paths)
-    if len(args.paths) == 0:
-        paths = ["."]
+    
     stat = dict()
-    for path in paths:
-        for root, dirs, files in os.walk(path):
-            for f in files:
-                ext = os.path.splitext(f)[1]
-                if ext not in stat:
-                    stat[ext] = 0
-                stat[ext] += 1
+    if len(args.path) == 0:
+        # read paths from stdin
+        text = read_stdin_text()
+        for line in text.split('\n'):
+            line = line.rstrip()
+            ext = os.path.splitext(line)[1]
+            if ext not in stat:
+                stat[ext] = 0
+            stat[ext] += 1
+    else:
+        # walk paths
+        paths = glob_paths_dirs(args.path)
+        for path in paths:
+            for root, dirs, files in os.walk(path):
+                for f in files:
+                    ext = os.path.splitext(f)[1]
+                    if ext not in stat:
+                        stat[ext] = 0
+                    stat[ext] += 1
+
     total = sum(stat.values())
     stat_ = [(ext, count) for ext, count in stat.items()]
     stat_.sort(key= lambda item: item[1], reverse=True)
