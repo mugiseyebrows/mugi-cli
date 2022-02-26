824b194cfa804a9e74123258d8a39bf064f69aab
Doronin Stanislav <mugisbrows@gmail.com>
2022-02-26 21:03:00 +0300

    zip a

--- mugicli/pyzip.py
+++ mugicli/pyzip.py
@@ -1,11 +1,11 @@
 import os
 import argparse
 import zipfile
-from .shared import print_utf8
+from .shared import print_utf8, glob_paths
 
 def main():
     parser = argparse.ArgumentParser(description='appends, extracts and list contents of zip archive')
-    parser.add_argument('command', choices=['a','x','l'])
+    parser.add_argument('command', choices=['a','x','l'], help='add extract list')
     parser.add_argument('-o', '--output', help='output directory')
     parser.add_argument('zip')
     parser.add_argument('sources', nargs='*') # todo wildcards
@@ -14,7 +14,16 @@ def main():
     dst_base = args.output if args.output is not None else os.getcwd()
 
     if args.command == 'a':
-        raise Exception("Not implemented")
+        paths = glob_paths(args.sources)
+        with zipfile.ZipFile(args.zip, 'a') as zf:
+            for path in paths:
+                #base = os.path.dirname(path)
+                base = os.getcwd()
+                for root, dirs, files in os.walk(path):
+                    for f in files:
+                        p = os.path.join(root, f)
+                        relpath = os.path.relpath(p, base)
+                        zf.write(p, relpath)
 
     if args.command == 'x':
         with zipfile.ZipFile(args.zip) as zf:
@@ -24,10 +33,11 @@ def main():
                     #dest = os.path.join(dst_base, name)
                     print("extract {} to {}".format(name, dst_base))
                     zf.extract(name, dst_base)
+                    
     if args.command == 'l':
         with zipfile.ZipFile(args.zip) as zf:
             for name in zf.namelist():
-                print_utf8(name + "\n")
+                print_utf8(name)
 
 if __name__ == "__main__":
     main()
