5c50ecd6405f4a1faccf26acad1f24099b497bd6
Doronin Stanislav <mugisbrows@gmail.com>
2022-03-09 01:09:23 +0300

    xargs -L -I

--- mugicli/pyxargs.py
+++ mugicli/pyxargs.py
@@ -20,26 +20,33 @@ def is_int(s):
 
 def print_help():
     print(textwrap.dedent("""\
-    usage: pyxargs [-n num] command [args...]
+    usage: pyxargs [-L NUM] [-I VAR] command [args...]
 
     reads arguments from stdin and applies them to command
 
     optional arguments:
-    -h --help  show this message and exit
-    -n         split arguments to chunks of n
+      -h --help  show this message and exit
+      -L NUM     split arguments to chunks of N args
+      -I VAR     replace VAR with arg (assumes -L 1)
+
     """))
 
 def main():
-    opts, args1 = parse_args(['h'],['help'],['n'],[], sys.argv[1:])
+    opts, args1 = parse_args(['h'],['help'],['L','I'],[], sys.argv[1:])
     if opts['h'] or opts['help']:
         print_help()
         return
     args2 = read_stdin_args()
-    if opts['n'] is None:
-        run(args1 + args2)
-    else:
+
+    if opts['L'] is not None:
         for args in chunks(args2, int(opts['n'])):
             run(args1 + args)
+    elif opts['I'] is not None:
+        for arg in args2:
+            cmd = [a.replace(opts['I'], arg) for a in args1]
+            run(cmd)
+    else:
+        run(args1 + args2)
 
 if __name__ == "__main__":
     main()
