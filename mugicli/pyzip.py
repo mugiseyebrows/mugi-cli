d56c12383fece3252c5dda0f4ca983d8bf67e713
Doronin Stanislav <mugisbrows@gmail.com>
2022-03-10 15:52:32 +0300

    compression methods and levels

--- mugicli/pyzip.py
+++ mugicli/pyzip.py
@@ -1,30 +1,55 @@
 import os
 import argparse
 import zipfile
+from zipfile import ZIP_STORED, ZIP_DEFLATED, ZIP_BZIP2, ZIP_LZMA
 from .shared import print_utf8, glob_paths
+import sys
 
 def main():
+
+    COMPRESSION_METHODS = {
+        'deflate': ZIP_DEFLATED,
+        'd': ZIP_DEFLATED,
+        'lzma': ZIP_LZMA,
+        'l': ZIP_LZMA,
+        'bzip2': ZIP_BZIP2,
+        'b': ZIP_BZIP2,
+        'store': ZIP_STORED,
+        's': ZIP_STORED
+    }
+
     parser = argparse.ArgumentParser(description='appends, extracts and list contents of zip archive')
     parser.add_argument('command', choices=['a','x','l'], help='add extract list')
     parser.add_argument('-o', '--output', help='output directory')
+    parser.add_argument('-m', choices=list(COMPRESSION_METHODS.keys()), help='compression method')
+    parser.add_argument('-l', type=int, help="compression level 0..9 for deflate (0 - best speed, 9 - best compression) 1..9 for bzip2, has no effect if method is store or lzma")
+    parser.add_argument('--base', help='base directory')
     parser.add_argument('zip')
     parser.add_argument('sources', nargs='*') # todo wildcards
     args = parser.parse_args()
 
     dst_base = args.output if args.output is not None else os.getcwd()
 
+    compresslevel = args.l
+
     if args.command == 'a':
         paths = glob_paths(args.sources)
-        with zipfile.ZipFile(args.zip, 'a') as zf:
+        compression = COMPRESSION_METHODS[args.m] if args.m is not None else ZIP_DEFLATED
+        with zipfile.ZipFile(args.zip, 'a', compression=compression, compresslevel=compresslevel) as zf:
             for path in paths:
-                #base = os.path.dirname(path)
-                base = os.getcwd()
+                if args.base is None:
+                    if os.path.isabs(path):
+                        base = os.path.dirname(path)
+                    else:
+                        base = os.getcwd()
+                else:
+                    base = args.base
                 for root, dirs, files in os.walk(path):
                     for f in files:
                         p = os.path.join(root, f)
                         relpath = os.path.relpath(p, base)
                         zf.write(p, relpath)
-
+                        
     if args.command == 'x':
         with zipfile.ZipFile(args.zip) as zf:
             for name in zf.namelist():
