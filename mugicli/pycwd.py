ad241a367d0bf065292417a79bea9a2b4928da48
Doronin Stanislav <mugisbrows@gmail.com>
2022-03-12 10:50:43 +0300

    cwd help

--- mugicli/pycwd.py
+++ mugicli/pycwd.py
@@ -1,6 +1,20 @@
 from .shared import print_utf8
 import os
+import sys
+import textwrap
+
+def print_help():
+    print(textwrap.dedent("""\
+    usage: pycwd [-h] [--help]
+
+    prints current working directory
+    """))
+
 def main():
+    args = sys.argv[1:]
+    if '-h' in args or '--help' in args:
+        print_help()
+        return
     print_utf8(os.getcwd())
 if __name__ == "__main__":
     main()
