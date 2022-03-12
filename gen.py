d444d53a902e161999af3014977e22a9077edd69
Doronin Stanislav <mugisbrows@gmail.com>
2022-03-12 10:51:40 +0300

    gen: usage script name fix

--- gen.py
+++ gen.py
@@ -17,6 +17,7 @@ with open(path, 'w', encoding='utf-8') as f:
 helps = dict()
 
 for prog in progs:
+    print(prog)
     helps[prog] = subprocess.check_output(["python","-m","mugicli.py" + prog,"--help"])
 
 data = dict()
@@ -29,6 +30,12 @@ for n in os.listdir('.'):
     with open(n, encoding='utf-8') as f:
         data[b] = f.read()
 
+def repl(m):
+    line = m.group(0)
+    name_py = m.group(1)
+    name = os.path.splitext(name_py)[0]
+    return line.replace(name_py, name)
+
 path = os.path.join(base, 'readme.md')
 with open(path, 'w', encoding='utf-8') as f:
     f.write(data['about'])
@@ -36,7 +43,11 @@ with open(path, 'w', encoding='utf-8') as f:
     for prog in progs:
         prog_ = 'py' + prog
         f.write("## {}\n```\n".format(prog_))
-        f.write(helps[prog].decode('utf-8').replace('\r\n','\n') + '\n')
+
+        cont = helps[prog].decode('utf-8').replace('\r\n','\n') + '\n'
+        cont = re.sub('usage: ([a-z0-9_]+\\.py)', repl, cont)
+
+        f.write(cont)
         f.write('```\n')
     f.write("# Notes\n")
     f.write(data['escaping'])
