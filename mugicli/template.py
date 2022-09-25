2497f85d21943a5478c74f8eab4089a3574a84cc
Doronin Stanislav <mugisbrows@gmail.com>
2022-09-25 16:21:41 +0300

    template

---
+++ mugicli/template.py
@@ -0,0 +1,12 @@
+import argparse
+
+def main():
+    EXAMPLE_TEXT = """examples:
+"""
+
+    parser = argparse.ArgumentParser(prog="", description="", epilog=EXAMPLE_TEXT, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("","",help="")
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
