0a367d00b4e0fae19790ffeff7f4d3bbe4f053b4
Doronin Stanislav <mugisbrows@gmail.com>
2023-11-08 18:32:39 +0300

    pyexec redirects

--- mugicli/pyexec/__init__.py
+++ mugicli/pyexec/__init__.py
@@ -30,6 +30,52 @@ examples:
 
 # todo pipe stdout to file or process
 
+from dataclasses import dataclass
+from typing import Any
+
+@dataclass
+class Redir:
+    file: Any = None
+    append: bool = False
+    join: bool = False
+    
+def redicrects(cmd):
+    cmd_ = cmd[:]
+    stderr = Redir()
+    stdout = Redir()
+    if len(cmd_) > 0:
+        if cmd_[-1] == '2>&1':
+            stderr.join = True
+            cmd_.pop(-1)
+        elif cmd_[-1] == '1>&2':
+            stdout.join = True
+            cmd_.pop(-1)
+
+    while len(cmd_) > 1:
+        ok = False
+        if cmd_[-2] in ['1>', '>']:
+            stdout.file = cmd_[-1]
+            stdout.append = False
+            ok = True
+        elif cmd_[-2] in ['1>>', '>>']:
+            stdout.file = cmd_[-1]
+            stdout.append = True
+            ok = True
+        elif cmd_[-2] == '2>':
+            stderr.file = cmd_[-1]
+            stderr.append = False
+            ok = True
+        elif cmd_[-2] == '2>>':
+            stderr.file = cmd_[-1]
+            stderr.append = True
+            ok = True
+        if ok:
+            cmd_.pop(-1)
+            cmd_.pop(-1)
+        else:
+            break
+    return cmd_, stdout, stderr
+
 def main():
     args = sys.argv[1:]
     if len(args) == 1 and args[0] in ['-h', '--help']:
@@ -59,9 +105,31 @@ def main():
                 if cmd == []:
                     debug_print('cmd == []')
                     continue
-                cmd_ = adjust_command(cmd)
-                debug_print("executing", cmd_)
-                proc = subprocess.run(cmd_)
+                cmd_, stdout_redir, stderr_redir = redicrects(cmd)
+                #print(cmd_, stdout, stderr)
+
+                cmd_ = adjust_command(cmd_)
+                stdout = None
+                stderr = None
+                
+                if stdout_redir.file:
+                    mode = "ab" if stdout_redir.append else "wb"
+                    stdout = open(stdout_redir.file, mode)
+                if stderr_redir.file:
+                    mode = "ab" if stderr_redir.append else "wb"
+                    stderr = open(stderr_redir.file, mode)
+
+                # stdout.join is 1>&2
+                if stdout_redir.join:
+                    stdout = stderr
+                if stderr_redir.join:
+                    stderr = stdout
+                debug_print("executing", cmd_, "stdout", stdout, "stderr", stderr)
+                proc = subprocess.run(cmd_, stderr=stderr, stdout=stdout)
+                if stdout:
+                    stdout.close()
+                if stderr:
+                    stderr.close()
                 returncode = proc.returncode
                 debug_print("returncode", returncode)
 
--- mugicli/pyexec/split.py
+++ mugicli/pyexec/split.py
@@ -35,9 +35,19 @@ def space_split(args):
                 res.append(e_)
     return res
 
+def aggregate_append(args):
+    pops = []
+    for i, arg in enumerate(args):
+        if arg == '>' and i+1 < len(args) and args[i+1] == '>':
+            pops.append(i+1)
+            args[i] == '>>'
+    for i in reversed(pops):
+        args.pop(i)
+    return args
+
 def and_or_split(args):
-    separators = ['&&', '||', ';']
-    separators_re = ['\\s*&&\\s*', '\\s*[|][|]\\s*', '\\s*;\\s*']
+    separators = ['&&', '||', ';', '>']
+    separators_re = ['\\s*&&\\s*', '\\s*[|][|]\\s*', '\\s*;\\s*', None]
     #print("args", args)
     for sep, sep_re in zip(separators, separators_re):
         res = []
@@ -45,11 +55,26 @@ def and_or_split(args):
             if len(e) > 0 and e[0] == "'":
                 res.append(e)
             else:
-                cols = re.split(sep_re, e)
-                res.append(cols[0])
-                for col in cols[1:]:
-                    res.append(sep)
-                    res.append(col)
+                if sep == '>':
+                    if e in ['2>&1', '1>&2']:
+                        res.append(e)
+                        continue
+                    m1 = re.match('([^>]*)(>[>]?)(.*)', e)
+                    m2 = re.match('([^>]*)([12]>[>]?)(.*)', e)
+                    if m2:
+                        cols = [m2.group(i) for i in range(1,4)]
+                    elif m1:
+                        cols = [m1.group(i) for i in range(1,4)]
+                    else:
+                        cols = [e]
+                    for col in cols:
+                        res.append(col)
+                else:
+                    cols = re.split(sep_re, e)
+                    res.append(cols[0])
+                    for col in cols[1:]:
+                        res.append(sep)
+                        res.append(col)
         args = res
     return args
 
@@ -66,43 +91,95 @@ def exec_split(args):
 
 def test_exec_split():
 
-    if 1:
+    group1 = 0
+
+    if group1:
         s = "a'b'c"
         e = ['a','b','c']
         a = exec_split([s])
         assert e == a
 
-    if 1:
+    if group1:
         s = "a 'b' c"
         e = ['a','b','c']
         a = exec_split([s])
         assert e == a
 
-    if 1:
+    if group1:
         s = "a;'b'&&c"
         e = ['a', ';','b','&&','c']
         a = exec_split([s])
         assert e == a
 
-    if 1:
+    if group1:
         s = "oud quc'ponix'atite ngrma'wns'kere ly ate"
         e = ["oud","quc",'ponix',"atite","ngrma",'wns',"kere","ly","ate"]
         a = exec_split([s])
         assert e == a
 
-    if 1:
+    if group1:
         s = "and po 'cus ff' chict idke 'bi wons've cog"
         e = ["and", "po", "cus ff", "chict", "idke", "bi wons", "ve", "cog"]
         a = exec_split([s])
         assert e == a
 
-    if 1:
+    if group1:
         s = "st; oze 'lica|| ons' pa&&qugaz 'nis ;tre';wasox || paly"
         e = ["st", ";", "oze", "lica|| ons", "pa", "&&", "qugaz", "nis ;tre", ";", "wasox", "||", "paly"]
         a = exec_split([s])
         assert e == a
 
+    if 1:
+        s = "echo 1 > test.txt"
+        e = ['echo', '1', '>', 'test.txt']
+        a = exec_split([s])
+        assert e == a
+
+    if 1:
+        s = "echo 1> test.txt"
+        e = ['echo', '1>', 'test.txt']
+        a = exec_split([s])
+        assert e == a
+
+    if 1:
+        s = "echo 3> test.txt"
+        e = ['echo', '3', '>', 'test.txt']
+        a = exec_split([s])
+        assert e == a
+
+    if 1:
+        s = "echo 1 2>&1"
+        e = ["echo","1","2>&1"]
+        a = exec_split([s])
+        assert e == a
+
+    if 1:
+        s = "echo 1>>1"
+        e = ["echo","1>>", "1"]
+        a = exec_split([s])
+        assert e == a
+
+    if 1:
+        s = "echo 2>>1"
+        e = ["echo","2>>", "1"]
+        a = exec_split([s])
+        assert e == a
+
+    if 1:
+        s = "echo 3>>1"
+        e = ["echo", "3", ">>", "1"]
+        a = exec_split([s])
+        assert e == a
+
+    if 1:
+        s = "echo 2 >> 1"
+        e = ["echo", "2", ">>", "1"]
+        a = exec_split([s])
+        assert e == a
+
     print("test_exec_split passed")
+
+test_exec_split()
 
 def split_cmds(args):
     cmd = []
