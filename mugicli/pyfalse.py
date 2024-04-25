ee5827f81c13fb4ee1b5ee6f7a7b62b89d32c5ff
Doronin Stanislav <mugisbrows@gmail.com>
2024-04-25 10:34:04 +0300

    fix pyfalse

--- mugicli/pyfalse.py
+++ mugicli/pyfalse.py
@@ -1,5 +1,8 @@
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(prog='pyfalse', description='exits with code 1')
+    parser.parse_args()
     exit(1)
 
 if __name__ == "__main__":
