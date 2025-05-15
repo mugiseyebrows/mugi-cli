6e3e9c652b454eb4d726228df76d63ac671b765b
Doronin Stanislav <mugisbrows@gmail.com>
2025-05-15 14:31:43 +0300

    pyuptime rewrite

--- mugicli/pyuptime.py
+++ mugicli/pyuptime.py
@@ -1,32 +1,38 @@
-try:
-    import win32api
-except ImportError:
-    pass
 import datetime
 import argparse
+
+HAS_PSUTIL = False
+try:
+    import psutil
+    HAS_PSUTIL = True
+except ImportError:
+    pass
+
+def todhms(td: datetime.timedelta):
+    s = td.total_seconds()
+    d = int(s // (24 * 3600))
+    s -= d * 24 * 3600
+    h = int(s // 3600)
+    s -= h * 3600
+    m = int(s // 60)
+    s -= m * 60
+    return d,h,m,int(s)
 
 def main():
 
     parser = argparse.ArgumentParser(description='prints uptime information')
-    parser.add_argument('-p', '--pretty', action='store_true', help='pretty print')
     args = parser.parse_args()
 
-    tickCount = win32api.GetTickCount()
-    delta = datetime.timedelta(milliseconds=tickCount)
-    s = delta.seconds
-    h = s // 3600
-    s -= h * 3600
-    m = s // 60
-    s -= m * 60
+    if not HAS_PSUTIL:
+        print("pyuptime requires psutil: pip install psutil")
+        return
+    
+    boot_date = datetime.datetime.fromtimestamp(psutil.boot_time())
+    now = datetime.datetime.now()
+    d,h,m,s = todhms(now - boot_date)
 
-    if args.pretty:
-        res = []
-        if delta.days > 0:
-            res.append("{:d} days".format(delta.days))
-        res.append("{:d} hours {:d} minutes {:d} seconds".format(h, m, s))
-        print(" ".join(res))
-    else:
-        print("{:02d}:{:02d}:{:02d}:{:02d}".format(delta.days, h, m, s))
+    print(f"booted {boot_date}")
+    print(f"uptime {d} days and {h:02d}:{m:02d}:{s:02d}")
 
 if __name__ == "__main__":
     main()
